1b4282030139adc5f83cf1667644cc1c74a010c7
iLLviA <illvia.3594@gmail.com>
2019-03-23 18:11:03 +0900

    update

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                a
+            </div>
+        )
+    }
+}
+
+render(<App/>, document.getElementById('app'))
